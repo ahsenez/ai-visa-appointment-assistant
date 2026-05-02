225b55db1d58922bc1f7a820cf6d7687fbc16807
ahsen ezgün <ahsen_ezgun@hotmail.com>
2026-05-02 15:19:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function analyzeAppointments() {
   const country = document.getElementById("country").value;
   const visaType = document.getElementById("visaType").value;
   const city = document.getElementById("city").value;
+  
  const appointmentList = document.getElementById("appointmentList");
   const aiText = document.getElementById("aiText");
 
